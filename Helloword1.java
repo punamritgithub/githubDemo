8ab70c0d8612a21bd3d60f193265939532f78be4
punamr <punamjondhale1994@gmail.com>
2022-08-02 07:57:27 -0700

    added helloword.java filehelloword.java

---
+++ Helloword1.java
@@ -0,0 +1,13 @@
+public class Helloword1{
+     public static void main (String arg []){
+         while (true){
+            System.out. println("welcome to Terminal commannds");
+              try{
+                 Thread.sleep(60000);
+                 }catch(Exception ex){}
+             }
+           }
+
+
+       }
+
