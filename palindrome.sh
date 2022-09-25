72e9a82629eddf4661bc2e703b794615e6193bad
punamr <punamjondhale1994@gmail.com>
2022-09-25 11:36:56 -0700

    Third commit

---
+++ palindrome.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+echo Enter a number
+read n
+temp=$n
+rev=0
+while [$n -gt 0]
+do
+a=`expr $n % 10`
+n=`expr $n / 10`
+
+rev= `expr $rev \* 10 + $a`
+done
+echo $rev
+if [ $temp -eq $rev ]
+then
+echo number is palindrome
+else
+echo number is not palindrome
+fi
+
+
